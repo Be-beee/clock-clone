b72d305d4fa3814944d319b1a040a7d5db0f95b2
bokyungseo <maybutter756@gmail.com>
2021-12-08 21:18:33 +0900

    AddWTController 상세 구현

--- Clock_clone/WorldTime/AddWTController.swift
+++ Clock_clone/WorldTime/AddWTController.swift
@@ -9,17 +9,15 @@ import UIKit
 
 class AddWTController: UIViewController {
     
-    var curLocation = Locale(identifier: "ko_KR").regionCode
-    var locations: [String] = []
+    var locations: [(lc: String, tz: String)] = [("서울, 대한민국", "KST"), ("도쿄, 일본", "TST"), ("뉴욕, 미국", "ET"), ("런던, 잉글랜드", "GMT")]
+    var selectedLocation: (lc: String, tz: String) = ("서울", "KST")
     
-
     @IBOutlet weak var wtSearchBar: UISearchBar!
     @IBOutlet weak var allwtTableView: UITableView!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(curLocation ?? "nil")
     }
 
     @IBAction func dismissView(_ sender: Any) {
@@ -28,17 +26,28 @@ class AddWTController: UIViewController {
 }
 
 extension AddWTController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 45
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return locations.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = allwtTableView.dequeueReusableCell(withIdentifier: "regionCell", for: indexPath) as? RegionCell else { return UITableViewCell() }
+        cell.locationLabel.text = locations[indexPath.row].lc
         
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        self.selectedLocation = locations[indexPath.row]
+        self.performSegue(withIdentifier: "toWTTable", sender: self)
     }
 }
 
 class RegionCell: UITableViewCell {
-    
+    @IBOutlet weak var locationLabel: UILabel!
 }
--- Clock_clone/WorldTime/WorldTimeViewController.swift
+++ Clock_clone/WorldTime/WorldTimeViewController.swift
@@ -9,16 +9,28 @@ import UIKit
 
 class WorldTimeViewController: UIViewController {
 
-    var worldTimeTable: [(location: String, time: Date)] = [("서울", Date()), ("서울", Date()), ("서울", Date())]
+    var worldTimeTable: [(lc: String, tz: String)] = []
     @IBOutlet weak var worldTimeTableView: UITableView!
-    
+    @IBOutlet weak var editTableButton: UIBarButtonItem!
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    @IBAction func unwindToWTTable(_ segue: UIStoryboardSegue) {
+        guard let addVC = segue.source as? AddWTController else { return }
+        self.worldTimeTable.append(addVC.selectedLocation)
+        self.worldTimeTableView.reloadData()
+    }
 
     @IBAction func editWorldTimeTable(_ sender: Any) {
-        
+        if self.worldTimeTableView.isEditing {
+            self.editTableButton.title = "편집"
+            self.worldTimeTableView.setEditing(false, animated: true)
+        } else {
+            self.editTableButton.title = "완료"
+            self.worldTimeTableView.setEditing(true, animated: true)
+        }
     }
     
     @IBAction func addWorldTime(_ sender: Any) {
@@ -43,23 +55,31 @@ extension WorldTimeViewController: UITableViewDelegate, UITableViewDataSource {
             return UITableViewCell()
         }
         
-        cell.locationLabel.text = worldTimeTable[indexPath.row].location
+        let city = worldTimeTable[indexPath.row].lc.split(separator: ",")[0]
+        cell.locationLabel.text = String(city)
         cell.timeCalCell.text = "오늘, +0시간"
         
         let df = DateFormatter()
         df.dateFormat = "a hh:mm"
-        let timeText = df.string(from: worldTimeTable[indexPath.row].time)
+        df.timeZone = TimeZone(abbreviation: worldTimeTable[indexPath.row].tz)
+        let timeText = df.string(from: Date())
         cell.timeLabel.text = timeText
         
         return cell
     }
     
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            self.worldTimeTable.remove(at: indexPath.row)
+            self.worldTimeTableView.reloadData()
+        }
+    }
     
-//    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-//        if editingStyle == .delete {
-//
-//        }
-//    }
+    func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        let target = self.worldTimeTable[sourceIndexPath.row]
+        self.worldTimeTable.remove(at: sourceIndexPath.row)
+        self.worldTimeTable.insert(target, at: destinationIndexPath.row)
+    }
     
 }
 
