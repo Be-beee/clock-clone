9e3026e3095189afe33b139cc5d420832755b56f
bokyungseo <maybutter756@gmail.com>
2021-12-25 22:51:15 +0900

    Add StopWatchVC

---
+++ Clock_clone/Stopwatch/StopWatchViewController.swift
@@ -0,0 +1,29 @@
+//
+//  StopWatchViewController.swift
+//  Clock_clone
+//
+//  Created by 서보경 on 2021/12/25.
+//
+
+import UIKit
+
+class StopWatchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
