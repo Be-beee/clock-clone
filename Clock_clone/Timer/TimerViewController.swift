7066b35b059a2d45bfbbdb0444c37231e193974f
bokyungseo <maybutter756@gmail.com>
2021-12-30 21:05:16 +0900

    setting timer button layer

--- Clock_clone/Timer/TimerViewController.swift
+++ Clock_clone/Timer/TimerViewController.swift
@@ -9,7 +9,26 @@ import UIKit
 
 class TimerViewController: UIViewController {
     
+    @IBOutlet weak var cancelButton: UIButton!
+    @IBOutlet weak var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setButtonlayers()
+    }
+    
+    func setButtonlayers(){
+        cancelButton.clipsToBounds = true
+        startButton.clipsToBounds = true
+        cancelButton.layer.cornerRadius = 40
+        startButton.layer.cornerRadius = 40
+    }
+    
+    @IBAction func cancelTimer(_ sender: Any) {
+        print("cancel timer")
+    }
+    
+    @IBAction func startTimer(_ sender: Any) {
+        print("start timer")
     }
 }
